0117b695bec83b2d872afd4874707e87e1b734c4
YarmoshIgop <57217503+YarmoshIgop@users.noreply.github.com>
2021-03-07 19:29:26 +0200

    ready to work with database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 
@@ -17,14 +18,13 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended: true}))
+
 app.use('/',homeRoutes)
 app.use('/add',addRoutes)
 app.use('/courses',coursesRoutes)
-
-
-
+app.use("/card", cardRoutes)
 
 const PORT = process.env.PORT || 3000;
 
--- routes/add.js
+++ routes/add.js
@@ -1,17 +1,20 @@
-const {Router} = require("express")
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
 router.get("/", (req, res) => {
-    res.render("add", {
-      title: "Додати курс",
-      isAdd: true
-    });
+  res.render("add", {
+    title: "Додати курс",
+    isAdd: true,
+  });
 });
 
-router.post('/',(req,res) => {
-    console.log(req.body);
+router.post("/", async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.img);
+  console.log(req.body);
+  await course.save();
 
-    res.redirect('/courses')
-})
+  res.redirect("/courses");
+});
 
-module.exports = router
+module.exports = router;
